86a39e1962a8833c39e3e72c111c37501ad9503f
Elias Bencz <eliasbencz@gmail.com>
2021-05-29 13:34:14 -0300

    Change printf by ncurses.

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 
 CFLAGS = -Werror -Wall
+LDLIBS = -lncurses
 
 main: main.o game.o gamec.o
--- gamec.c
+++ gamec.c
@@ -1,14 +1,10 @@
+#include <ncurses.h>
 #include "gamec.h"
-#include <stdio.h>
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+#define HEARTS     1
+#define CUBS     2
+#define DIAMONDS  3
+#define SPADES    4
 
 
 static void print_card(const struct card *card)
@@ -28,23 +24,15 @@ static void print_card(const struct card *card)
 		"Queen",
 		"King",
 	};
-#if 0
-	static const char *suits[MAX_SUITS] = {
-		"Hearts",
-		"Spade",
-		"Clubs",
-		"Diamonds",
+	static int suits[MAX_SUITS] = {
+		HEARTS,
+		CUBS,
+		DIAMONDS,
+		SPADES,
 	};
-	printf("%s %s\n", names[card->value], suits[card->suit]);
-#else
-	static const char *suits[MAX_SUITS] = {
-		KRED,
-		KNRM,
-		KYEL,
-		KMAG,
-	};
-	printf("%s%s\n", suits[card->suit], names[card->value]);
-#endif
+	attron(COLOR_PAIR(suits[card->suit]));
+	printw("%s\n", names[card->value]);
+	attroff(COLOR_PAIR(suits[card->suit]));
 }
 
 static void print_cards(const struct card * const cards[], int num_cards)
@@ -53,7 +41,6 @@ static void print_cards(const struct card * const cards[], int num_cards)
 	for (i = 0; i < num_cards; i++) {
 		print_card(cards[i]);
 	}	
-	printf(KNRM);
 }
 
 void deck_print(const struct deck *deck)
@@ -71,19 +58,30 @@ void stack_print(const struct stack *stack)
 	print_cards(stack->pile, stack->count);
 }
 
+
 void tableau_print(const struct tableau *tableau)
 {
 	int i;
+	initscr();
+
+	start_color();
+	init_pair(HEARTS, COLOR_RED, COLOR_MAGENTA);
+	init_pair(DIAMONDS, COLOR_YELLOW, COLOR_GREEN);
+	init_pair(CUBS, COLOR_CYAN, COLOR_BLUE);
+	init_pair(SPADES, COLOR_BLACK, COLOR_WHITE);
+
 	for (i = 0; i < NUM_STACKS; i++) {
-		printf("#####################\n");
-		printf("STACK-%d\n", i);
+		printw("#####################\n");
+		printw("STACK-%d\n", i);
 		stack_print(&tableau->stacks[i]);
-		printf("#####################\n");
+		printw("#####################\n");
 	}
-	printf("#####################\n");
-	printf("STOCK\n\n");
+	printw("#####################\n");
+	printw("STOCK\n\n");
 	stock_print(&tableau->stock);
-	printf("#####################\n");
+	printw("#####################\n");
+
+	refresh();
+	getch();
+	endwin();
 }
-
-
